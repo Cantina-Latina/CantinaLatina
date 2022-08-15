99544bae7d71eabaafb8d70473a1d837c85513c5
pmbohm <pmbohm@gmail.com>
2022-08-15 10:48:29 +0000

    Deploying to gh-pages from @ Cantina-Latina/site@b7d46f0f9dccd73dad5c7f323d3f1700be93ee1c 🚀

---
+++ _next/static/chunks/pages/about-e3f2d1fb9e5a229e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{6515:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(6248)}])}},function(n){n.O(0,[17,248,774,888,179],(function(){return _=6515,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
