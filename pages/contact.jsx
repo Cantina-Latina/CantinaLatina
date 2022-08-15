af7b358ee0de3a441f2abbfa9ee9b098a048752c
littlebrothergeorge <pmbohm@utas.edu.au>
2022-08-15 22:55:40 +1000

    fix contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,4 @@
-import {  Typography } from "@mui/material"
+import { Typography } from "@mui/material"
 import { Box } from "@mui/material"
 import { SocialsList } from "../components/socialsList"
 import { ParallaxPageBox } from "../components/parallaxPageBox"
@@ -13,38 +13,38 @@ export default function Contact({ children }) {
         <>
 
 
-                <main>
+            <main>
 
-                
-                    <ParallaxPageBox id="contact"  bgColor={"#595959"} >
-                        <Box sx={{ 
-                padding: '60px 90px',
-                borderRadius: '4px'
-                }} >
-                        <Typography variant="h3" md={12}  lg={3}  style={{color : "#FFFFFF", margin: '0px'}} >
-                                Any enquiries please send us an email. </Typography>
+
+                <ParallaxPageBox id="contact" bgColor={"#595959"} >
+                    <Box sx={{
+                        padding: { lg: '60px 90px', xs: '20px 90px' },
+                        borderRadius: '4px'
+                    }} >
+                        <Typography variant="h3" md={12} lg={3} style={{ color: "#FFFFFF", margin: '0px' }} >
+                            Any enquiries please send us an email. </Typography>
                         <Grid2 container
                             spacing={6}
                             direction="row"
                             justifyContent="flex-start"
                             alignItems="center"
-                          
+
                         >
                             <Grid2 md={12} lg={4}>
                                 <SocialsList textcolor="white" />
                             </Grid2>
 
                             <Grid2 md={8} lg={8}>
-                                <Typography variant="h4" style={{color : "#D69C1E"}} >Follow us on Facebook and Instagram.
+                                <Typography variant="h4" style={{ color: "#D69C1E" }} >Follow us on Facebook and Instagram.
                                 </Typography>
                             </Grid2>
 
 
                         </Grid2>
 
-                </Box>
-                    </ParallaxPageBox>
-                </main>
+                    </Box>
+                </ParallaxPageBox>
+            </main>
 
 
 
